e061c99f361e86984118022ecdddbe44b7d10932
Gustavo Martínez <gustavo@codeman.company>
2018-08-05 22:29:54 -0500

    Progress Darwin

--- src/Providers/MercadoPagoServiceProvider.php
+++ src/Providers/MercadoPagoServiceProvider.php
@@ -11,14 +11,17 @@ class MercadoPagoServiceProvider extends ServiceProvider {
 	protected $mp_app_secret;
 
 	public function boot() {
-		
-		$this -> publishes( [
-			__DIR__ . '/../config/mercadopago.php' => config_path( 'mercadopago.php' )
-		] );
+		// $this -> publishes( [
+		// 	__DIR__ . '/../config/mercadopago.php' => config_path( 'mercadopago.php' ),
+		// ] );
 
-		$this -> mp_app_access_token = config( 'mercadopago.app_access_token' );
-		$this -> mp_app_id = config( 'mercadopago.app_id' );
-		$this -> mp_app_secret = config( 'mercadopago.app_secret' );
+		// $this -> mp_app_access_token = config( 'mercadopago.app_access_token' );
+		// $this -> mp_app_id = config( 'mercadopago.app_id' );
+		// $this -> mp_app_secret = config( 'mercadopago.app_secret' );
+
+		$this -> mp_app_access_token = env( 'MP_APP_ACCESS_TOKEN', '' );
+		$this -> mp_app_id = env( 'MP_APP_ID', '' );
+		$this -> mp_app_secret = env( 'MP_APP_SECRET', '' );
 	}	// end method
 
 	public function register() {
